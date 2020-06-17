6f80d9cacd36d598166f43de86e1f5120595a0fe
shmakovpn <shmakovpn@yandex.ru>
2020-06-17 11:42:37 +0300

    Added importing subpackages to django_adtools package (changed from . to from django_adtools)

--- django_adtools/__init__.py
+++ django_adtools/__init__.py
@@ -4,5 +4,5 @@ django_adtools/__init__.py
 __author__ = 'shmakovpn <shmakovpn@yandex.ru>'
 __date__ = '2020-06-05'
 
-from . import ad
-from . import dns
+from django_adtools import ad
+from django_adtools import dns
