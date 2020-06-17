5556897b97e0c3e386cbaf2f6586af8562e6395b
shmakovpn <shmakovpn@yandex.ru>
2020-06-17 11:48:13 +0300

    changed from to import

--- django_adtools/__init__.py
+++ django_adtools/__init__.py
@@ -4,5 +4,7 @@ django_adtools/__init__.py
 __author__ = 'shmakovpn <shmakovpn@yandex.ru>'
 __date__ = '2020-06-05'
 
-from django_adtools import ad
-from django_adtools import dns
+import django_adtools.ad
+import django_adtools.dns
+
+
